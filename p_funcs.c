8e9b4bd90cf26573f45d6b3863dc1c1f68eae932
oladele90 <oladelemiro07@gmail.com>
2022-11-18 12:09:48 -0800

    file of functions added push function

---
+++ p_funcs.c
@@ -0,0 +1,20 @@
+/**
+ *
+ *
+ *
+ */
+
+void push_func(stack_t **first_node, unsigned int line_number)
+{
+	stack_t *new;
+
+	new = malloc(sizeof(stack_t));
+	if (first_node != NULL)
+	{
+		new->next = first_node;
+		first_node->prev = new;
+	}
+	new->prev = NULL;
+	new->n = num;
+	first_node = new;
+}
