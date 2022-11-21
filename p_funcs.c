e3ec86aa8d1837b2b412e3e8f8d5fbd61a61b7b6
oladele90 <oladelemiro07@gmail.com>
2022-11-21 12:46:58 -0800

    pall update

--- p_funcs.c
+++ p_funcs.c
@@ -12,6 +12,11 @@ void push_op(stack_t **first_node, unsigned int line_number)
 	stack_t *new;
 	(void) line_number;
 	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 	new->n = num;
 	new->prev = NULL;
 	if (first_node != NULL && *first_node != NULL)
@@ -49,10 +54,11 @@ void free_stack(stack_t *first_node)
 
 void pall_op(stack_t **first_node, unsigned int line_number)
 {
+	stack_t *my = *first_node;
 	(void) line_number;
-	while (*first_node != NULL)
+	while (my != NULL)
 	{
-		printf("%d\n", (*first_node)->n);
-		*first_node = (*first_node)->next;
+		printf("%d\n", my->n);
+		my = my->next;
 	}
 }
