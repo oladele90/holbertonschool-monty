f2e695fd128c47b0e3ba0bca70d5e0e42be3f5c9
oladele90 <oladelemiro07@gmail.com>
2022-11-21 09:21:06 -0800

    free stack func

--- p_funcs.c
+++ p_funcs.c
@@ -12,12 +12,33 @@ void push_op(stack_t **first_node, unsigned int line_number)
 	stack_t *new;
 	(void) line_number;
 	new = malloc(sizeof(stack_t));
+	new->n = num;
+	new->prev = NULL;
 	if (first_node != NULL)
 	{
 		new->next = *first_node;
 		(*first_node)->prev = new;
 	}
-	new->prev = NULL;
-	new->n = num;
+	else
+		(new->next = NULL);
 	*first_node = new;
+}
+
+/**
+ *
+ */
+
+void free_stack(stack_t *first_node)
+{
+	stack_t *temp;
+
+	if (first_node == NULL)
+		return;
+	while (first_node != NULL)
+	{
+		temp = first_node;
+		first_node = first_node->next;
+		free(temp);
+	}
+	free(first_node);
 }
